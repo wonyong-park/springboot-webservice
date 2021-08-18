7326d88ec2b0a79fc65c8299b0245cad139ea979
wonyong-park <wanyong0919@naver.com>
2021-08-18 12:24:42 +0900

    2021-08-18 삭제 로직 만들기

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,6 +7,9 @@ var main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save: function () {
         var data = {
@@ -31,7 +34,7 @@ var main = {
     update: function () {
         var data = {
             title: $('#title').val(),
-            content: $('#title').val()
+            content: $('#content').val()
         };
 
         var id = $('#id').val();
@@ -44,6 +47,21 @@ var main = {
             data: JSON.stringify(data)
         }).done(function () {
             alert("글이 수정되었습니다.");
+            window.location.href = "/";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
             window.location.href = "/";
         }).fail(function (error) {
             alert(JSON.stringify(error));
