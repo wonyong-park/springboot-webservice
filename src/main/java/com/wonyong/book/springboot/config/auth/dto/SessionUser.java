012e8979bb2fdb6bfd6a36b05809af0f510f0931
wonyong-park <wanyong0919@naver.com>
2021-08-20 17:26:50 +0900

    2021-08-20 Serializable 추가

--- src/main/java/com/wonyong/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/wonyong/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.wonyong.book.springboot.config.auth.dto;
 import com.wonyong.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
