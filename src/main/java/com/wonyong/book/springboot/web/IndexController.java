34b6edb9e1d51729b4c0b0feebe4e323d0afd319
wonyong-park <wanyong0919@naver.com>
2021-08-20 16:46:00 +0900

    2021-08-20 어노테이션 기반으로 세션정보가져오기

--- src/main/java/com/wonyong/book/springboot/web/IndexController.java
+++ src/main/java/com/wonyong/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.wonyong.book.springboot.web;
 
+import com.wonyong.book.springboot.config.auth.LoginUser;
 import com.wonyong.book.springboot.config.auth.dto.SessionUser;
 import com.wonyong.book.springboot.service.posts.PostsService;
 import com.wonyong.book.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
